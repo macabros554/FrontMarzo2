64eced325474a1c8e9f615ea554a05f2257e0a72
macabros554 <macabros554@gmail.com>
2022-03-05 13:46:36 +0100

    enlace envio cambiado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://backmarzoheroku.herokuapp.com/'
+  baseUrl: 'https://backmarzoheroku.herokuapp.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://backmarzoheroku.herokuapp.com/'
+  baseUrl: 'https://backmarzoheroku.herokuapp.com'
 };
 
 /*
